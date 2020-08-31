7f8bf9aed398b4bb6582a6fa50083f8e9202db59
dmitri-mcguckin <28746912+dmitri-mcguckin@users.noreply.github.com>
2020-08-31 01:46:49 +0000

    Deploying to gh-pages from  @ 512a2a7cf0b6eeadd69b996b47e54e5132e0b7d9 🚀

---
+++ portfolio/assets/manifests/projects.ts
@@ -0,0 +1,22 @@
+const assets_dir = '/assets/project-imgs/';
+
+export const projects = [
+  {
+    image: assets_dir + 'canopen-monitor.png',
+    title: 'OreSat CANOpen Monitor',
+    url: 'canopen-monitor',
+    category: 'major'
+  },
+  {
+    image: assets_dir + 'oresat-ultra.png',
+    title: 'OreSat UniClOGS Live Telemetry API',
+    url: 'ultra',
+    category: 'major'
+  },
+  {
+    image: assets_dir + 'rusty-baduk.png',
+    title: 'Rusty Baduk',
+    url: 'rusty-baduk',
+    category: 'major'
+  }
+]
---
+++ portfolio/assets/manifests/socials.ts
@@ -0,0 +1,14 @@
+export const socials = [
+  {
+    type: 'twitter',
+    url: 'https://twitter.com/DmitriMcguckin'
+  },
+  {
+    type: 'github',
+    url: 'https://github.com/dmitri-mcguckin'
+  },
+  {
+    type: 'linkedin',
+    url: 'https://www.linkedin.com/in/dmitrimcguckin'
+  },
+]
